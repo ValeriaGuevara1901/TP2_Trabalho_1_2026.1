eaa00bf07023fcc44adb944bdbf2efc698fd2247
SENIOR\valeria.guevara <valeria.guevara@senior.com.br>
2026-04-13 19:30:07 -0300

    TDD Testes 19-24 REFACTOR: melhorar documentacao do romanos.hpp

--- romanos.hpp
+++ romanos.hpp
@@ -1,11 +1,14 @@
 ﻿// Copyright 2026 Valeria Guevara
-// Declaracao da funcao de conversao de numeros romanos
+// Modulo de conversao de numeros romanos para arabicos.
+// Suporta numeros de 1 a 3000; strings de ate 30 caracteres.
 
 #ifndef ROMANOS_HPP_
 #define ROMANOS_HPP_
 
-// Converte string de numero romano para inteiro.
-// Retorna -1 se invalido.
+// Converte string de numero romano para inteiro arabico.
+// Recebe: num_romano - string com simbolos {I,V,X,L,C,D,M} (ate 30 chars)
+// Retorna: inteiro correspondente, ou -1 se a entrada for invalida
+//          (vazia, chars desconhecidos, repeticoes excessivas, valor > 3000)
 int romanos_para_decimal(char const * num_romano);
 
 #endif  // ROMANOS_HPP_
