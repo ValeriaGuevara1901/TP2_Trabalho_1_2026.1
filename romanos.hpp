a925e0be2c329a88439007f4973a7b3d58b6b7cf
SENIOR\valeria.guevara <valeria.guevara@senior.com.br>
2026-04-13 19:23:02 -0300

    TDD: estrutura inicial - romanos.hpp com guard, stub cpp, arquivo de testes vazio

--- romanos.hpp
+++ romanos.hpp
@@ -1,6 +1,11 @@
-#include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
-{
-  return 0; 
-}
- 
+﻿// Copyright 2026 Valeria Guevara
+// Declaracao da funcao de conversao de numeros romanos
+
+#ifndef ROMANOS_HPP_
+#define ROMANOS_HPP_
+
+// Converte string de numero romano para inteiro.
+// Retorna -1 se invalido.
+int romanos_para_decimal(char const * num_romano);
+
+#endif  // ROMANOS_HPP_
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -1,2 +1,8 @@
-int romanos_para_decimal(char const * num_romano);
- 
+﻿// Copyright 2026 Valeria Guevara
+// Testes TDD para conversao de numeros romanos
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "romanos.hpp"
+
+// Testes serao adicionados progressivamente via TDD
